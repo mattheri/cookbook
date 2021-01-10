d21703e052bc9057b9027e44540e5683bccadf3d
Mathieu Thériault <mathieu.theriault89@gmail.com>
2021-01-10 12:15:59 -0500

    add popup overlay to logout and access profile page

--- components/Context/AppContext.tsx
+++ components/Context/AppContext.tsx
@@ -3,15 +3,28 @@ import { useGetAuthCookie } from "../Hooks/useGetAuthCookie";
 
 export const AppContext = React.createContext(null);
 
+export type User = {
+    id: string,
+    username: string,
+    name?: string,
+    picture?: string
+}
+
+export type AppContextState = {
+    connected: boolean,
+    user: User
+}
+
 export function AppContextProvider<T>(props: React.PropsWithChildren<T>) {
 
     const user = useGetAuthCookie();
 
-    const [appState, setAppState] = React.useState({
-        id: user ? user.id : "",
+    const [appState, setAppState] = React.useState<AppContextState>({
         connected: user ? user.connected : false,
         user: user ? user.user : {}
     });
+
+    React.useEffect(() => console.log(appState), [appState]);
 
     return (
         <AppContext.Provider value={[appState, setAppState]}>
--- components/ProfileButton/ProfilePopup.tsx
+++ components/ProfileButton/ProfilePopup.tsx
@@ -3,14 +3,55 @@ import Link from "next/link";
 import styles from "./profilebutton.module.css";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
+import { useUserLogOut } from "../Hooks/useUserLogOut";
+import axios from "axios";
 
-export function ProfilePopup() {
+type Props = {
+    id: string
+}
+
+type User = {
+    id: string,
+    username: string
+    name?: string,
+    picture?: string,
+}
+
+export function ProfilePopup({ id }: Props) {
+    const [user, setUser] = React.useState<User>({
+        id: "",
+        username: "",
+        name: '',
+        picture: ''
+    }); 
+
+    React.useEffect(() => {
+        (async () => {
+            const res = await (await axios.get(`/api/user/${id}`)).data;
+            setUser(res);
+        })()
+    }, [])
+
+    const handleLogOut = useUserLogOut();
 
     return (
-        <Popup trigger={<div className={styles.profilebtn}>
-        </div>} position="bottom center">
-            Hello theeereeeeeeee
-
+        <Popup
+            arrow
+            on="hover"
+            contentStyle={{
+                display: "flex",
+                flexDirection: "column",
+                zIndex: 10000
+            }}
+            trigger={
+                <a className={styles.profilebtn}>
+                    {user.username && user.picture ? <img src={user.picture} /> : user.username.substring(0, 1).toUpperCase()}
+                </a>
+            }
+            mouseLeaveDelay={300}
+            position="bottom left">
+            <Link href={`/profile/${id}`}><a className={styles.profile}>Profile</a></Link>
+            <a className={styles.logout} onClick={handleLogOut}>Logout</a>
         </Popup>
     );
 }
