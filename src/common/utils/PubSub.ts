9d321eaa88eb11576a249f4490fb42387556d02f
Mathieu Thériault <mathieu.theriault89@gmail.com>
2022-08-18 19:30:28 -0400

    Await all callbacks in pubsub

--- src/common/utils/PubSub.ts
+++ src/common/utils/PubSub.ts
@@ -4,8 +4,6 @@ import { injectable } from "inversify";
 type Callback = (...args: any[]) => any | Promise<any>;
 type Callbacks = Record<string, Set<Callback>>;
 type Unsubscribe = () => void;
-const ASYNC_FUNCTION_SYMBOL = "AsyncFunction";
-type FunctionWithSymbol = { [x: symbol]: string };
 
 @injectable()
 class Pubsub<T> {
@@ -33,12 +31,7 @@ class Pubsub<T> {
   publish(event: T, ...args: any[]) {
     const ev = event as unknown as string;
     this.callbacks[ev]?.forEach(async (cb) => {
-      const tempCb = cb as unknown as FunctionWithSymbol;
-      const functionType: string = tempCb[Symbol.toStringTag];
-
-      if (functionType.includes(ASYNC_FUNCTION_SYMBOL))
-        await cb.apply(cb, args);
-      else cb.apply(cb, args);
+      await cb.apply(cb, args);
     });
   }
 }
